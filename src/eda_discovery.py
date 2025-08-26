a82b5345acb745a0c4a89c1e324ae0e7c875e72b
BEDAYA-ACA <tarkkhatib@gmail.com>
2025-08-26 21:07:47 +0300

    update

--- src/eda_discovery.py
+++ src/eda_discovery.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.close()  # close any previous plots
 
-
 df = pd.read_csv("ab_testing_analytics/data/processed/daily_stats.csv")
 
 print(df.head(5))
@@ -17,8 +16,9 @@ df_treatment = df[df['group_name'] == 'treatment']
 df_control = df[df['group_name'] == 'control']
 
 
+
 # Plot both groups on the same chart
-plt.close()
+plt.close() # close any previous plots
 plt.figure(figsize=(12, 6))
 
 plt.plot(df_treatment['date'], df_treatment['conversion_rate'], 
@@ -31,7 +31,20 @@ plt.title('Daily Conversion Rates: Treatment vs Control')
 plt.xlabel('Date')
 plt.ylabel('Conversion Rate (%)')
 plt.xticks(rotation=45)
-plt.legend()   # عشان يبين مين الخط الأزرق ومين الأحمر
+plt.legend()  
 plt.tight_layout()
 plt.savefig("ab_testing_analytics/figures/daily_conversion_rates_compare.png")
+plt.close()
+
+
+# plot overall conversion rates for both groups
+plt.close() # close any previous plots
+plt.figure(figsize=(8, 6))
+plt.bar(['Treatment', 'Control'], 
+        [df_treatment['conversion_rate'].mean(), df_control['conversion_rate'].mean()], 
+        color=['blue', 'red'])
+plt.title('Overall Conversion Rates: Treatment vs Control')
+plt.ylabel('Average Conversion Rate (%)')
+plt.tight_layout()
+plt.savefig("ab_testing_analytics/figures/overall_conversion_rates_compare.png")
 plt.close()
